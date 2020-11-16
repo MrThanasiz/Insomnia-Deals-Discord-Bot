da2b029101086c555267a8d80541b76aa9c070a3
MrThanasiz <thanasiz99@gmail.com>
2020-11-16 23:02:25 +0100

    Listener kinda working, code needs a good cleaning :')

--- bot.py
+++ bot.py
@@ -1,18 +1,44 @@
 import discord
+import dealsListener
+import asyncio
+from threading import Thread
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await checkDeals()
+    
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$setChannel'):
+        print(str(message.channel.id))
+        await message.channel.send("Channel set: " + "<#" + str(message.channel.id) + ">")
+        channelList.append(message.channel.id)
+        sendDeals()
+
+channelList = []
+def sendDeals():
+    print(channelList)
+
+async def sendMessageToChannel(channelID, message):
+    channel = client.get_channel(channelID)
+    await channel.send(message)
+
+async def checkDeals():
+    d = dealsListener.checkForDeals()
+    recentPosts = [d[0]]
+    newPosts = [d[1]]
+    if len(channelList) > 0:
+        for channelID in channelList:
+            await sendMessageToChannel(channelID, recentPosts[0]["title"])
+    await asyncio.sleep(10)
+    await checkDeals()
 
 try:
     #read token
@@ -22,5 +48,8 @@ try:
 except:
     print("something wrong with the token...")
 else:
+    #dealsListener = Thread(target = dealsListener.checkForDeals, args = [3])
+    #discord = Thread(target = client.run, args = [token])
+    #discord.start()
+    #dealsListener.start()
     client.run(token)
-
--- dealsListener.py
+++ dealsListener.py
@@ -1,12 +1,76 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse("demo.xml")
+import urllib.request
+import asyncio
+import time
 
-root = tree.getroot()
-channel = root[0]
+def getLatestGuid(channel):
+    for post in channel:
+        if post.tag == "item":
+            for info in post:
+                if info.tag == "guid":
+                    return(info.text)
 
-for post in channel:
-    if post.tag == "item":
-        for info in post:
-            print(info.tag)
-            print(info.text)
-            #print((info.text).encode("utf-8"))
+def getPostData(post):
+    postData = {}
+    postData["title"] = post[0].text
+    postData["link"] = post[1].text
+    postData["desc"] = post[2].text
+    postData["guid"] = post[3].text
+    postData["date"] = post[4].text
+    return postData
+
+def getRecentPosts(channel, recentPosts): #gets and returns latest page of posts
+    recentPosts = []
+    for post in channel:
+        if post.tag == "item":
+            postData = getPostData(post)
+            recentPosts.append(postData)
+    return recentPosts
+
+def getNewPosts(channel, recentPosts):   #gets & returns new posts not in list
+    newPosts = []
+    for post in channel:
+        if post.tag == "item":
+            postData = getPostData(post)
+            if postData["guid"] in recentPosts:
+                break
+            else:
+                newPosts.append(postData)
+                recentPosts.append(postData)
+    return [recentPosts, newPosts]
+
+async def postsListener():
+    print("ok...")
+
+
+url = "https://www.insomnia.gr/forums/forum/56-%CF%80%CF%81%CE%BF%CF%83%CF%86%CE%BF%CF%81%CE%AD%CF%82.xml/?sortby=start_date&sortdirection=desc"
+
+def parseURL(url):
+    opener = urllib.request.build_opener()
+    opener.addheaders = []
+    tree = ET.parse(opener.open(url))
+    root = tree.getroot()
+    return root[0]
+
+guid = 0
+def checkForDeals():
+    channel = parseURL(url)
+    print("Latest GUID" + getLatestGuid(channel))
+    recentPosts = []
+    recentPosts, newPosts = getNewPosts(channel, recentPosts)
+    print(recentPosts[0]["title"])
+    return recentPosts
+
+
+#recentPosts = []
+#channel = parseURL(url)
+#recentPosts = []
+#recentPosts, newPosts = getNewPosts(channel, recentPosts)
+#print("Latest post: " + str(recentPosts[0]))
+#print("Latest GUID" + getLatestGuid(channel))
+##############################
+
+async def main():
+    await checkForDeals(5)
+
+#asyncio.run(main())
